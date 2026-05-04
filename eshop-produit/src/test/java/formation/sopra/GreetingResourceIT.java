92bad06b7cb7c09d73a058099ed07dc07428f4d7
xavierfrhf-fr <xavier.frhf@gmail.com>
2026-05-04 10:18:30 +0200

    Création projet PRODUIT

---
+++ eshop-produit/src/test/java/formation/sopra/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package formation.sopra;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
