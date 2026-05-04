7892e4f4c638d0d53030b5d5f55559af72ce9c81
Marie <malestro56@gmail.com>
2026-05-04 11:48:12 +0200

    creation commande + produit + request

--- service-commande/src/main/api/dto/request/CreateCommandeRequest.java
+++ service-commande/src/main/api/dto/request/CreateCommandeRequest.java
@@ -1,3 +1,5 @@
-public class CreateCommandeRequest {
+package main.api.dto.request;
+
+public record CreateCommandeRequest (Integer clientId, List<Produit> produits, double total) {
     
 }
---
+++ service-commande/src/main/model/Produit.java
@@ -0,0 +1,7 @@
+package main.model;
+
+import java.math.BigDecimal;
+
+public record Produit (String nom, BigDecimal prix){
+    
+}
