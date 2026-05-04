de4dbd6e36b0967a99e2107c95d7230fffde8c45
AxelleDrd <axelle.drouard@orange.fr>
2026-05-04 14:32:05 +0200

    Ajout findCommandeByClientId à CommandeRepository

--- service-commande/src/main/repo/CommandeRepository.java
+++ service-commande/src/main/repo/CommandeRepository.java
@@ -6,5 +6,10 @@ import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class CommandeRepository implements PanacheRepositoryBase<Commande, Integer> {
-
+        
+    public List<Integer> findCommandeByClientId(String clientId) {
+        return this.find("select id from commande c where c.clientId = ?1", clientId)
+        .project(Integer.class).list();
+    }
+    
 }
